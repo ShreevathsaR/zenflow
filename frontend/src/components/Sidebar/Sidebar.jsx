a04f19fcc50e7aa41d710634a9c4e40b9a3b5629
TunedMajor08 <103721643+TunedMajor08@users.noreply.github.com>
2024-08-04 23:23:49 +0530

    Sidebar and color theme changes

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,32 +1,47 @@
-import React, { useState } from 'react'
-import './Sidebar.css'
-import { SlOptions } from "react-icons/sl";
-import { FaAnglesLeft } from "react-icons/fa6";
-import { usePageContext } from '../Contexts/PageContext';
+import React, { useState } from "react";
+import "./Sidebar.css";
+import { FaUserCircle } from "react-icons/fa";
+import { IoNotifications } from "react-icons/io5";
+import { usePageContext } from "../Contexts/PageContext";
+import { MdDashboard } from "react-icons/md";
+import { MdViewKanban } from "react-icons/md";
+import { BiChalkboard } from "react-icons/bi";
+import { RiTodoFill } from "react-icons/ri";
+import { RiTeamFill } from "react-icons/ri";
 
-const Sidebar = ({showSideBar,setShowSideBar}) => {
+const Sidebar = ({ showSideBar, setShowSideBar }) => {
+  console.log(setShowSideBar);
 
-    console.log(setShowSideBar)
+  const { page, setPage } = usePageContext();
 
-    const {page,setPage} = usePageContext();
+  const handleNotes = () => {
+    setPage("Notes");
+  };
 
-    const handleNotes = () => {
-        setPage("Notes")
-    }
-
-    const handleTodo = () => {
-        setPage("Todo")
-    }
+  const handleTodo = () => {
+    setPage("Todo");
+  };
 
   return (
-    <div className={`sidebar ${showSideBar ? '' : 'closed'}`}>
+    <div className={`sidebar ${showSideBar ? "" : "closed"}`}>
+      <div className="profile-section">
+        <FaUserCircle />
+        <div className="sidebar-username">Shreevathsa</div>
+        <div className="notifications">
+          <IoNotifications />
+        </div>
+      </div>
+      <div className="sidebar-options">
         <ul>
-            <div className="closing-bar"><FaAnglesLeft onClick={()=>{setShowSideBar(false)}}/></div>
-            <li onClick={handleNotes}>Notes <SlOptions/></li>
-            <li onClick={handleTodo}>Todo <SlOptions/></li>
+          <li><MdDashboard style={{paddingRight:"0.4rem"}}/>Home</li>
+          <li onClick={()=>{handleNotes()}}><MdViewKanban style={{paddingRight:"0.4rem"}}/>Kanban</li>
+          <li><RiTodoFill style={{paddingRight:"0.4rem"}}/>Todo</li>
+          <li><BiChalkboard style={{paddingRight:"0.4rem"}}/>Whiteboard</li>
+          <li><RiTeamFill style={{paddingRight:"0.4rem"}}/>Organizations</li>
         </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;

fd149f421684b8103643284678dd01f4ac2aa9e2
TunedMajor08 <103721643+TunedMajor08@users.noreply.github.com>
2024-08-05 23:14:38 +0530

    Solved merger conflicts

--- frontend/src/supabaseClient.jsx
+++ frontend/src/supabaseClient.jsx
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://oybxpwsjoycqfdiwvyfq.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95Ynhwd3Nqb3ljcWZkaXd2eWZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI4NjM1NjAsImV4cCI6MjAzODQzOTU2MH0.OCEtNovvDW6-svhXlx99hyZ-Ew4DeWfOmYqt-kJ0Ltw'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
-=======
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://wwpvfhirmuktiuhvqjel.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3cHZmaGlybXVrdGl1aHZxamVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI2NjUwMzksImV4cCI6MjAzODI0MTAzOX0.0U-Q60t29rTA8nfA-tS9TPxZMnmkDu3KDjs8ZVKcCdU';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
->>>>>>> 57504640dc136f15aa315aa01cc29f7c3302a927
